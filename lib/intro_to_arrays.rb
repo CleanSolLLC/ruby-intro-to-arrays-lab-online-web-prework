86175b0e71e82cfb6be2fb74e9b1d250ef172f72
Mark May <mamark90@gmail.com>
2019-03-07 20:39:00 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -10,16 +10,16 @@ end
 taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
 
 def first_element(taylor_swift)
-  my_first_element = first_element[0]
+  my_first_element = taylor_swift[0]
 end
 
 def third_element(taylor_swift)
-  my_third_element = third_element[2]
+  my_third_element = taylor_swift[2]
 end
 
 
 def last_element(taylor_swift)
-  my_last_element = last_element[-1]
+  my_last_element = taylor_swift[-1]
 end
 
 
